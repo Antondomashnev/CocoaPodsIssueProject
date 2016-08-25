45b4ffd5db415afdf8f8c5422670afe446bec802
Anton Domashnev <antondomashnev@gmail.com>
2016-08-25 22:26:45 +0200

    Initial Commit

---
+++ CocoaPodsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CocoaPodsTest
+//
+//  Created by Anton Domashnev on 25/08/16.
+//  Copyright © 2016 antondomashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
